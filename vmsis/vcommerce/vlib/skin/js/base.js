855c71b82dd0f9a65b2b9be0b11f132429aaccda
jeffersongouvea <jefferson@vmsis.com.br>
2015-04-08 03:20:45 -0300

    Atualização de layout

---
+++ vmsis/vcommerce/vlib/skin/js/base.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('input').addClass('form-control');
+  $('select').addClass('form-control');
+  $('textarea').addClass('form-control');
+});
---
+++ vmsis/vcommerce/vlib/skin/js/login.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('input').addClass('form-control');
+  $('#id_username').attr('placeholder','Usuário');  
+  $('#id_password').attr('placeholder','Senha');
+});
