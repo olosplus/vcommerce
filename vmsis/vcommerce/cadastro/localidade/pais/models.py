d9c5f9265059c8cc393f116c1079f262fdfd1b8c
jeffersongouvea <jefferson@vmsis.com.br>
2015-04-20 04:39:13 -0300

    Alteração start.py e inclusão de países.

--- vmsis/vcommerce/cadastro/localidade/pais/models.py
+++ vmsis/vcommerce/cadastro/localidade/pais/models.py
@@ -9,9 +9,14 @@ class Pais(models.Model):
 		verbose_name = "País"
 		verbose_name_plural = "Países"
 
-	cdpais = models.CharField(max_length=10,verbose_name='Código',unique=True,db_index=True)
-	sgpais = models.CharField(max_length=2,verbose_name='Sigla',unique=True,db_index=True)
-	nmpais = models.CharField(max_length=250,verbose_name='Nome')
+	cdpais = models.CharField(max_length=5,verbose_name='Código IBGE',unique=True,db_index=True)
+	cdsiscomex = models.CharField(max_length=5,verbose_name='Código SISCOMEX',db_index=True, null=True)
+	sgpais2 = models.CharField(max_length=2,verbose_name='Sigla',db_index=True, null=True)
+	sgpais3 = models.CharField(max_length=3,verbose_name='Sigla 3 letras',db_index=True, null=True)
+	nmpais = models.CharField(max_length=250,verbose_name='País', null=False)
+
+	class Meta:
+		ordering = ['nmpais']
 
 	def __str__(self):
 		return self.nmpais
--- vmsis/vcommerce/start.py
+++ vmsis/vcommerce/start.py
@@ -1,18 +1,30 @@
 #!/opt/workspace/vtestes/vmsis/bin/python3.3
 # -*- coding: utf8 -*- 
 
-import subprocess
+import subprocess, os
 
-print("\nGerenciando a evolução dos models e schemas...")
-return_code = subprocess.call('python manage.py migrate', shell=True)
-print("\nConferindo sincronização e criando usuário principal caso não exista...")
-return_code = subprocess.call('python manage.py syncdb', shell=True)
-print("\nIdentificando e criando os arquivos de migração")
-return_code = subprocess.call('python manage.py makemigrations', shell=True)
+try:
+    print(input("\nAtenção, não use o start em produção! (Ctrl+C para sair)\n"))
+    if (os.path.exists('db.sqlite3')):
+    	if input("Deseja excluir o banco de dados existente? (Default=N)").capitalize()=="S":
+    		print("\nRemovendo banco de dados...")
+    		return_code = subprocess.call('rm db.sqlite3', shell=True)
+    
+    print("\nIdentificando e criando os arquivos de migração")
+    return_code = subprocess.call('python manage.py makemigrations', shell=True)
+    print("\nGerenciando a evolução dos models e schemas...")
+    return_code = subprocess.call('python manage.py migrate', shell=True)
+    
+    if input("Deseja criar um super usuário? (Default=N)").capitalize()=="S":
+        print("\nCriando usuário principal...")
+        return_code = subprocess.call('python manage.py createsuperuser', shell=True)
 
-print("\n\nAlimentando tabelas:")
-print("\nPaís...")
-return_code = subprocess.call('python manage.py loaddata cadastro/fix/pais.json', shell=True)
+    print("\n\nAlimentando tabelas:")
+    print("\nPaís...")
+    return_code = subprocess.call('python manage.py loaddata cadastro/fix/pais.json', shell=True)
 
-print("\nIniciando serviço...")
-return_code = subprocess.call('python manage.py runserver', shell=True)
+    print("\nIniciando serviço...")
+    return_code = subprocess.call('python manage.py runserver', shell=True)
+
+except KeyboardInterrupt:
+	print("\nOperação cancelada!\n\n")
