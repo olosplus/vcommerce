59cfc594d51c93a868478d379a18f4014f6486e7
jeffersongouvea <jefferson@vmsis.com.br>
2015-05-05 00:12:22 -0300

    Update models.py

--- vmsis/vcommerce/cadastro/produto/models.py
+++ vmsis/vcommerce/cadastro/produto/models.py
@@ -15,7 +15,6 @@ class Produto(Master_empresa):
 	posarvore = models.CharField(max_length=40,verbose_name='Arvore')
 	nmproduto = models.CharField(max_length=200,verbose_name='Nome',blank=True)
 	unimedida = models.ForeignKey(Unimedida,verbose_name='Unidade de medida')
-	fatorconv = models.FloatField(verbose_name='Fator de conversão')
 	cdbarra = models.CharField(max_length=100,verbose_name='Código de barras',blank=True)
 	idprodvenda = models.BooleanField(verbose_name='Produto de venda',default=False,blank=True) 
 	idadicional = models.BooleanField(verbose_name='Adicional',default=False,blank=True)
