4b71917df53ef6e7a7b1ef1c91defcc04099c34f
jeffersongouvea <jefferson@vmsis.com.br>
2015-05-23 10:22:23 -0300

    Mensagem login colorida.

--- vmsis/vcommerce/vlib/skin/js/login.js
+++ vmsis/vcommerce/vlib/skin/js/login.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function() {  
+  $('.errorlist').addClass('alert alert-danger');
   $('input').addClass('form-control');
   $('#id_username').attr('placeholder','Usuário');  
   $('#id_password').attr('placeholder','Senha');
