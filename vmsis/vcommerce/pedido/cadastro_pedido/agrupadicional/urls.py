62b7f55fae866677b350cd294cdafe6d21be6c3a
MarinaAmaral <marina.amaral@vmsis.com.br>
2015-05-23 20:22:44 -0300

    adicional

--- vmsis/vcommerce/pedido/cadastro_pedido/agrupadicional/urls.py
+++ vmsis/vcommerce/pedido/cadastro_pedido/agrupadicional/urls.py
@@ -2,7 +2,8 @@
 from django.conf.urls import patterns, include, url
 from vlib.view_lib import CrudView
 from pedido.cadastro_pedido.agrupadicional.models import AgrupAdicional
+from pedido.cadastro_pedido.agrupadicional.views import ViewAgrupAdicionalCreate
 
 Crud = CrudView(AgrupAdicional)
 
-urlpatterns = Crud.AsUrl()
+urlpatterns = Crud.AsUrl(ClassCreate = ViewAgrupAdicionalCreate)
