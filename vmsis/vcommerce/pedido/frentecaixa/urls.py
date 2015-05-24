89b0e5f9c27450ffa37e8525b7575dba72041f89
MarinaAmaral <marina.amaral@vmsis.com.br>
2015-05-24 00:58:23 -0300

    update do pedido

--- vmsis/vcommerce/pedido/frentecaixa/urls.py
+++ vmsis/vcommerce/pedido/frentecaixa/urls.py
@@ -2,9 +2,9 @@
 from django.conf.urls import patterns, include, url
 from vlib.view_lib import CrudView
 from pedido.frentecaixa.models import Pedido
-from pedido.frentecaixa.views import ViewFrentecaixaCreate
+from pedido.frentecaixa.views import ViewFrentecaixaCreate, ViewFrentecaixaUpdate
 
 
 Crud = CrudView(Pedido)
 
-urlpatterns = Crud.AsUrl(MediaFilesInsert = ['js/pedido.js'],ClassCreate = ViewFrentecaixaCreate,GridFields  = ('idtipopedido','vrpedido'))
+urlpatterns = Crud.AsUrl(MediaFilesInsert = ['js/pedido.js'],MediaFilesUpdate = ['js/pedido.js'],ClassCreate = ViewFrentecaixaCreate,ClassUpdate = ViewFrentecaixaUpdate,GridFields  = ('idtipopedido','vrpedido'))
--- vmsis/vcommerce/pedido/frentecaixa/views.py
+++ vmsis/vcommerce/pedido/frentecaixa/views.py
@@ -12,34 +12,38 @@ class FormFrentecaixa(StandardFormGrid):
         model = Pedido
     
     def before_insert_grid_row(self, instance):
-    	print('passou')
-    	#if instance.__class__.__name__ = 'ItemPedido'
-    	if isinstance(instance, ItemPedido):
+        if isinstance(instance, ItemPedido):
     	    cardapio = Cardapio.objects.get(pk=instance.cardapio.id)
     	    instance.vrvenda = cardapio.vrvenda
     	    instance.vrtotal = instance.qtitem * instance.vrvenda
     	    
     def after_insert_grid_row(self, instance):
     	if isinstance(instance,ItAdicional):
-    		itempedido = ItemPedido.objects.get(pk=instance.itempedido.id)
-    		cardapio = Cardapio.objects.get(pk=itempedido.cardapio.id)
-    		itempedido.vrvenda = cardapio.vrvenda 
-    		adicionais = Adicionais.objects.get(pk=instance.item.id)
-    		agrupadicional = AgrupAdicional.objects.get(pk=adicionais.agrupadicional.id)
-    		itempedido.vrtotal = (itempedido.qtitem * cardapio.vrvenda) + (agrupadicional.vragrupadic*instance.qtitem)
-    		itempedido.save();
+            itempedido = ItemPedido.objects.get(pk=instance.itempedido.id)
+            cardapio = Cardapio.objects.get(pk=itempedido.cardapio.id)
+            itempedido.vrvenda = cardapio.vrvenda 
+            adicionais = Adicionais.objects.get(pk=instance.item.id)
+            agrupadicional = AgrupAdicional.objects.get(pk=adicionais.agrupadicional.id)
+            itempedido.vrtotal = (itempedido.qtitem * cardapio.vrvenda) + (itempedido.qtitem *(agrupadicional.vragrupadic*instance.qtitem))
+            itempedido.idacional = True
+            itempedido.save()
 
     def save(self, commit=True):
-    	instance = super(FormFrentecaixa, self).save(commit=True)
-    	itempedido = ItemPedido.objects.filter(pedido=instance)
-    	vrtotal = 0
-    	for item in itempedido:
-    		vrtotal += item.vrtotal
-    	instance.vrpedido = vrtotal
-    	instance.save() 
-    	
+        instance = super(FormFrentecaixa, self).save(commit=True)
+        itempedido = ItemPedido.objects.filter(pedido=instance)
+        vrtotal = 0
+        for item in itempedido:
+            vrtotal += item.vrtotal
+        instance.vrpedido = vrtotal
+        instance.idstatusped = 'C'
+        instance.save() 
+        return instance
     	
 
 
 class ViewFrentecaixaCreate(ViewCreate):
     form_class = FormFrentecaixa
+
+class ViewFrentecaixaUpdate(ViewUpdate):
+    form_class = FormFrentecaixa
+    
