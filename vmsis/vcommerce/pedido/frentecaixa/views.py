5bcead87cd94e64eadbde736cbb876d64ed895d9
MarinaAmaral <marina.amaral@vmsis.com.br>
2015-05-24 12:22:08 -0300

    alterando o update do pedido

--- vmsis/vcommerce/pedido/frentecaixa/views.py
+++ vmsis/vcommerce/pedido/frentecaixa/views.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- codaftertf-8 -*-
 from django.shortcuts import render
 from vlib.view_lib import ViewCreate, StandardFormGrid, ViewUpdate
 from pedido.frentecaixa.models import Pedido, ItemPedido, ItAdicional
@@ -13,9 +13,15 @@ class FormFrentecaixa(StandardFormGrid):
     
     def before_insert_grid_row(self, instance):
         if isinstance(instance, ItemPedido):
-    	    cardapio = Cardapio.objects.get(pk=instance.cardapio.id)
-    	    instance.vrvenda = cardapio.vrvenda
-    	    instance.vrtotal = instance.qtitem * instance.vrvenda
+            cardapio = Cardapio.objects.get(pk=instance.cardapio.id)
+            instance.vrvenda = cardapio.vrvenda
+            instance.vrtotal = instance.qtitem * instance.vrvenda
+            
+    def before_update_grid_row(self, instance, old_instance):
+        if isinstance(instance, ItemPedido):
+            cardapio = Cardapio.objects.get(pk=instance.cardapio.id)
+            instance.vrvenda = cardapio.vrvenda
+            instance.vrtotal = instance.qtitem * instance.vrvenda
     	    
     def after_insert_grid_row(self, instance):
     	if isinstance(instance,ItAdicional):
@@ -28,9 +34,19 @@ class FormFrentecaixa(StandardFormGrid):
             itempedido.idadicional = True
             itempedido.save()
 
+    def after_update_grid_row(self, instance,old_instance):
+        if isinstance(instance,ItAdicional):
+            itempedido = ItemPedido.objects.get(pk=instance.itempedido.id)
+            cardapio = Cardapio.objects.get(pk=itempedido.cardapio.id)
+            itempedido.vrvenda = cardapio.vrvenda 
+            adicionais = Adicionais.objects.get(pk=instance.item.id)
+            agrupadicional = AgrupAdicional.objects.get(pk=adicionais.agrupadicional.id)
+            itempedido.vrtotal = (itempedido.qtitem * cardapio.vrvenda) + (itempedido.qtitem *(agrupadicional.vragrupadic*instance.qtitem))
+            itempedido.idadicional = True
+            itempedido.save()
+
     def save(self, commit=True):
         instance = super(FormFrentecaixa, self).save(commit=True)
-        print('ali')
         itempedido = ItemPedido.objects.filter(pedido=instance)
         vrtotal = 0
         for item in itempedido:
@@ -38,7 +54,6 @@ class FormFrentecaixa(StandardFormGrid):
         instance.vrpedido = vrtotal
         instance.idstatusped = 'C'
         instance.save()
-        print(instance.vrpedido) 
         return instance
     	
 
