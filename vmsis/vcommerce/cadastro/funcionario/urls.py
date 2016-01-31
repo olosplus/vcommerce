89d7a59ab4f3bf099b3ca82802d788afa5f2957b
ivanmrosa <ivan@vmsis.com.br>
2016-01-31 16:50:17 -0200

    --Alteração para não exibir a senha ao editar

--- vmsis/vcommerce/cadastro/funcionario/urls.py
+++ vmsis/vcommerce/cadastro/funcionario/urls.py
@@ -2,8 +2,8 @@
 from django.conf.urls import patterns, include, url
 from vlib.view_lib import CrudView
 from cadastro.funcionario.models import Funcionario
-from cadastro.funcionario.views import ViewFuncionario
+from cadastro.funcionario.views import ViewFuncionario, ViewFuncionarioEdit
 
 Crud = CrudView(Funcionario)
 
-urlpatterns = Crud.AsUrl(GridFields  = ('nome','usuario',), ClassCreate = ViewFuncionario)
+urlpatterns = Crud.AsUrl(GridFields  = ('nome','usuario',), ClassCreate = ViewFuncionario, ClassUpdate = ViewFuncionarioEdit)
--- vmsis/vcommerce/cadastro/funcionario/views.py
+++ vmsis/vcommerce/cadastro/funcionario/views.py
@@ -1,16 +1,19 @@
  # -*- coding: utf-8 -*-
 from django import forms
-from vlib.view_lib import ViewCreate, StandardFormGrid, ViewDelete
+from vlib.view_lib import ViewCreate, ViewUpdate, StandardFormGrid, ViewDelete
 from cadastro.funcionario.models import Funcionario
 
 class FormFuncionario(StandardFormGrid):
     class Meta:
         model = Funcionario
 
-    senha = forms.CharField(max_length=30, widget=forms.PasswordInput(), label='Senha')
-    confsenha = forms.CharField(max_length=30, widget=forms.PasswordInput(), 
+    senha = forms.CharField(max_length=30, widget=forms.PasswordInput, label='Senha')
+    confsenha = forms.CharField(max_length=30, widget=forms.PasswordInput, 
        label='Confirma Senha')
     cep = forms.CharField(max_length=9, label="CEP", widget=forms.TextInput(attrs={'class':'cep'}))
 
 class ViewFuncionario(ViewCreate):
+    form_class = FormFuncionario
+
+class ViewFuncionarioEdit(ViewUpdate):
     form_class = FormFuncionario
